2c4ef786d798c5ddf711bcf84fc07a41c404236d
Ethan Curtis <95894855+UltraSpeedDemon@users.noreply.github.com>
2022-04-08 19:27:02 -0400

    Computer Build Application created

--- src/main/java/com/example/ethancurtis_assignment2comp1008/PCBuildController.java
+++ src/main/java/com/example/ethancurtis_assignment2comp1008/PCBuildController.java
@@ -1,15 +1,54 @@
 package com.example.ethancurtis_assignment2comp1008;
 
+import javafx.scene.control.TextField;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.image.Image;
 
 public class PCBuildController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Computer Build! ");
-    }
+    private Button addPart;
 
+    @FXML
+    private Button addWish;
+
+    @FXML
+    private Label brand;
+
+    @FXML
+    private Label locationPlace;
+
+    @FXML
+    private Label name;
+
+    @FXML
+    private Button nextPart;
+
+    @FXML
+    private AnchorPane numPad;
+
+    @FXML
+    private Label numParts;
+
+    @FXML
+    private Label part;
+
+    @FXML
+    private Label price;
+
+    @FXML
+    private Label value;
+
+    @FXML
+    private TextField wishName;
+
+    @FXML
+    private TextField wishPart;
+
+
+    //loads background
+    Image background = new Image(getClass().getResource("img/computerBuild.jpg").toExternalForm());
 }
