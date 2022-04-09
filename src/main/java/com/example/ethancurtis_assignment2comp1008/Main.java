44812eb1d3e23edf69b1065b7494218886f332c4
Ethan Curtis <95894855+UltraSpeedDemon@users.noreply.github.com>
2022-04-09 18:29:25 -0400

    Final Commit

--- src/main/java/com/example/ethancurtis_assignment2comp1008/Main.java
+++ src/main/java/com/example/ethancurtis_assignment2comp1008/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("pc-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 950, 425);
-        stage.setTitle("Computer Build");
+        stage.setTitle("Computer Build!");
         stage.setScene(scene);
         stage.show();
     }
