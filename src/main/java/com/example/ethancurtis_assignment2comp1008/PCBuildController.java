3021124eb25e5eedf526f5aca26614dc6a60d073
Ethan Curtis <95894855+UltraSpeedDemon@users.noreply.github.com>
2022-04-08 23:15:05 -0400

    Wishlist created, and Recent Wishes List

--- src/main/java/com/example/ethancurtis_assignment2comp1008/PCBuildController.java
+++ src/main/java/com/example/ethancurtis_assignment2comp1008/PCBuildController.java
@@ -14,13 +14,16 @@ import javafx.scene.image.Image;
 public class PCBuildController implements Initializable {
 
     @FXML
-    private Button addPart;
-
-    @FXML
-    private Button addWish;
-
-    @FXML
     private Label brand;
+
+    @FXML
+    private Button btnAddPart;
+
+    @FXML
+    private Button btnAddWish;
+
+    @FXML
+    private Button btnNextPart;
 
     @FXML
     private Label locationPlace;
@@ -29,7 +32,7 @@ public class PCBuildController implements Initializable {
     private Label name;
 
     @FXML
-    private Button nextPart;
+    private TextField nameWish;
 
     @FXML
     private AnchorPane numPad;
@@ -38,16 +41,28 @@ public class PCBuildController implements Initializable {
     private Label numParts;
 
     @FXML
-    private Label part;
+    private Label numWish;
 
     @FXML
-    private TextField partName;
+    private Label part;
 
     @FXML
     private TextField partWish;
 
     @FXML
     private Label price;
+
+    @FXML
+    private Label recentName;
+
+    @FXML
+    private Label recentNameHeader;
+
+    @FXML
+    private Label recentPart;
+
+    @FXML
+    private Label recentPartHeader;
 
     @FXML
     private Label value;
@@ -58,16 +73,16 @@ public class PCBuildController implements Initializable {
     /** Variable/Flag **/
     int i = 0;
 
-    /** initialize/Jumpstart the application with intial values**/
+    /** initialize/Jumpstart the application with initial values**/
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         Computer computerBuild = new Computer("510 Bryne Drive,ON L4N 9P6");
-        ElectronicStore part1 = new ElectronicStore("Nvidia","RTX 3070","Graphics Card",1199.99);
-        ElectronicStore part2 = new ElectronicStore("Cooler Master","Hyper 212 Black","CPU Fan",59.99);
-        ElectronicStore part3 = new ElectronicStore("Intel","I7-9700K","CPU",745.99);
-        ElectronicStore part4 = new ElectronicStore("Asus","Z-390 Prime","MotherBoard",329.99);
-        ElectronicStore part5 = new ElectronicStore("G-SKILL","Trident-Z 32GB","Ram",249.99);
-        ElectronicStore part6 = new ElectronicStore("Corsair","275R Mid-Tower","Case",109.99);
+        ElectronicStore part1 = new ElectronicStore("Nvidia","RTX 3070","Graphics Card",1199.19);
+        ElectronicStore part2 = new ElectronicStore("Cooler Master","Hyper 212 Black","CPU Fan",59.89);
+        ElectronicStore part3 = new ElectronicStore("Intel","I7-9700K","CPU",745.49);
+        ElectronicStore part4 = new ElectronicStore("Asus","Z-390 Prime","MotherBoard",329.29);
+        ElectronicStore part5 = new ElectronicStore("G-SKILL","Trident-Z 32GB","Ram",282.59);
+        ElectronicStore part6 = new ElectronicStore("Corsair","275R Mid-Tower","Case",109.29);
         computerBuild.addPart(part1);
         computerBuild.addPart(part2);
         computerBuild.addPart(part3);
@@ -94,11 +109,36 @@ public class PCBuildController implements Initializable {
         value.setText(Double.toString(computer.getComputerValue()));
         locationPlace.setText(computer.getLocation());
     }
+    int wishListTotal = 0;
+    @FXML
+    private void displayWishParts(Computer wishList){
+        ElectronicStore wishList1 = wishList.getParts(i);
+        recentPartHeader.setText("Part:");
+        recentNameHeader.setText("Name:");
+        recentPart.setText(wishList1.getPart());
+        recentName.setText(wishList1.getName());
+    }
+    @FXML
+    private void btnAddWishPushed()
+    {
+        int i = 0;
+        String wishPart = partWish.getText();
+        String wishName = nameWish.getText();
+        nameWish.setText("");
+        partWish.setText("");
+        Computer wishList = new Computer("510 Bryne Drive,ON L4N 9P6"); //same location
+        ElectronicStore wish1 = new ElectronicStore("TestingRandomWishBrand",wishName,wishPart,10.00);//random brand and price values -- List of Wishes
+            wishList.addPart(wish1);
+        wishListTotal = wishListTotal + 1;
+        numWish.setText(""+wishListTotal);
+
+        displayWishParts(wishList); /** Starts the display method to send to displayWishParts method with wishlist arraylist **/
+    }
     @FXML
     private void btnNextPartPushed(){
         if(i < 5){
             i++;
-            Computer computerBuild = new Computer("510 Bryne Drive Unit A-E, Barrie, ON L4N 9P6");
+            Computer computerBuild = new Computer("510 Bryne Drive,ON L4N 9P6");
             ElectronicStore part1 = new ElectronicStore("Nvidia","RTX 3070","Graphics Card",1199.99);
             ElectronicStore part2 = new ElectronicStore("Cooler Master","Hyper 212 Black","CPU Fan",59.99);
             ElectronicStore part3 = new ElectronicStore("Intel","I7-9700K","CPU",745.99);
